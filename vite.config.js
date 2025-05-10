fcbd05b9f0e859c74a7a3ee0b1a7da7b270e1bb1
TiagoSPimentel <tiago.pimentel111@gmail.com>
2025-05-10 19:38:41 -0400

    ajustando responsividade

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Expondo o servidor para todas as interfaces de rede
+    port: 5173,        // Ou qualquer outra porta que você queira
+  },
 })
